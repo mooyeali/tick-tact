1032cb3f42d381d53d00ae978a5ef562e55280c3
mooyeali <mooyeali@yeah.net>
2024-08-23 15:15:10 +0800

    txtfeat TickTact项目：初始化结构和依赖
    创建了TickTact项目的骨架，包括基本的Maven配置和依赖项。集成了Thymeleaf、Spring Boot、JPA、H2数据库和Lombok等技术栈。添加了初始的HTML文件和应用属性配置。

---
+++ src/main/kotlin/cn/com/mooyeali/ticktact/TickTactApplication.kt
@@ -0,0 +1,9 @@
+package cn.com.mooyeali.ticktact
+
+import org.springframework.boot.runApplication
+
+class TickTactApplication
+
+fun main(args: Array<String>) {
+    runApplication<TickTactApplication>(*args)
+}
---
+++ src/test/kotlin/cn/com/mooyeali/ticktact/TickTactApplicationTests.kt
@@ -0,0 +1,11 @@
+package cn.com.mooyeali.ticktact
+
+import org.junit.jupiter.api.Test
+
+class TickTactApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
